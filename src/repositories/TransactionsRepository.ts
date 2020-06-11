66a15188447d1fc8336962237afe99b2d90d088e
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-06-11 02:35:46 -0300

    Added code to getbalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,35 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (acc, curr) => {
+        let accumulated;
+        if (curr.type === 'income') {
+          accumulated = {
+            income: acc.income + Number(curr.value),
+            outcome: acc.outcome,
+            total: acc.total + Number(curr.value),
+          };
+        } else {
+          accumulated = {
+            income: acc.income,
+            outcome: acc.outcome + Number(curr.value),
+            total: acc.total - Number(curr.value),
+          };
+        }
+
+        return accumulated;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 }
 
